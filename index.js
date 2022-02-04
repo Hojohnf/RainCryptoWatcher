a3b63f015443abbf1793a4d8ecee8e7ac761e51c
paradisebike <paradisebike.asnieres@gmail.com>
2022-02-04 02:43:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+let price = require('crypto-price')
+
+app.get('/eth', (req, res) => {
+    console.log("ETH appelé")
+
+  price.getCryptoPrice("EUR", "ETH").then(obj => {
+    let value = obj.price.substring(0, 7)
+    res.send('<div>ETH : '+ value +'</div>')
+}).catch(err => {
+    console.log(err)
+})
+})
+
+app.get('/lpt', (req, res) => {
+    console.log("LPT appelé")
+
+  price.getCryptoPrice("EUR", "LPT").then(obj => {
+    let value = obj.price.substring(0, 5)
+    res.send('<div>LPT : '+ value +'</div>')
+}).catch(err => {
+    console.log(err)
+})
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
