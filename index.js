255fd0fa21a03ff244a1ee6bbb82eb7e53487d01
paradisebike <paradisebike.asnieres@gmail.com>
2022-02-06 23:45:32 +0100

    new api

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
-let price = require('crypto-price')
+const axios = require('axios');
 
 app.get('/eth', (req, res) => {
-    console.log("ETH appelé")
-
-  price.getCryptoPrice("EUR", "ETH").then(obj => {
-    let value = obj.price.substring(0, 7)
-    res.send('<div>ETH : '+ value +'</div>')
-}).catch(err => {
-    console.log(err)
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    const BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=ETH&convert=EUR"
+    response = await axios.get(BASE_URL, {
+      headers: {
+        'X-CMC_PRO_API_KEY': '0dee75a6-d363-4dd4-bc57-5359fdc7650b',
+      }
+    });
+  } catch(ex) {
+    response = null;
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    const json = response.data;
+    resolve(json);
+    res.send('<div>ETH : '+ json.data.ETH.quote.EUR.price.toString().substr(0, 7)+'</div>')
+  }
 })
-})
+}),
 
 app.get('/lpt', (req, res) => {
-    console.log("LPT appelé")
-
-  price.getCryptoPrice("EUR", "LPT").then(obj => {
-    let value = obj.price.substring(0, 5)
-    res.send('<div>LPT : '+ value +'</div>')
-}).catch(err => {
-    console.log(err)
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    const BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=LPT&convert=EUR"
+    response = await axios.get(BASE_URL, {
+      headers: {
+        'X-CMC_PRO_API_KEY': '0dee75a6-d363-4dd4-bc57-5359fdc7650b',
+      }
+    });
+  } catch(ex) {
+    response = null;
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    const json = response.data;
+    resolve(json);
+    res.send('<div>LPT : '+ json.data.LPT.quote.EUR.price.toString().substr(0, 5)+'</div>')
+  }
 })
 })
 
